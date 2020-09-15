d7d8b07a0de41c314995062ba98acfce65b13a98
SerbanConstantin <serban.g.constantin@gmail.com>
2020-09-16 02:17:07 +0300

    http

--- noteit-ng-app/src/app/app.module.ts
+++ noteit-ng-app/src/app/app.module.ts
@@ -4,15 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import { HttpClientModule} from "@angular/common/http";
 
+// @ts-ignore
+const appRoutes :Routes = [
+  {
+    path: 'notes',
+    component:NotesComponent
+  },
+  {
+    path:'',
+    component:NotesComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    NotesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
